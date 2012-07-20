33e26b85a4cd74911cc628112c3b67a3a85190d4
Jan Pecha <janpecha@email.cz>
2012-07-21 00:31:46 +0200

    [tools] minifier - update regexp (broken media queries)
    fix #2
    
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- tools/minifier/libs/CssMinifier.php
+++ tools/minifier/libs/CssMinifier.php
@@ -16,8 +16,6 @@
 		{
 			$s = preg_replace('#/\*.*?\*/#s', '', $s); // remove comments
 			$s = preg_replace('#\s+#', ' ', $s); // compress space
-			#$s = preg_replace('# ([^0-9a-z.\#*-])#i', '$1', $s);
-#FUNKCNI			$s = preg_replace('# ([^()0-9a-z.\#*-])#i', '$1', $s);
 			$s = preg_replace('# ([^(0-9a-z.\#*-])#i', '$1', $s);
 			$s = preg_replace('#([^0-9a-z%)]) #i', '$1', $s);
 			$s = str_replace(';}', '}', $s); // remove leading semicolon
